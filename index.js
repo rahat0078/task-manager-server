48d2c91ef129c013cb65039a075cf18f7635d945
rahat0078 <rarahat1710@gmail.com>
2025-02-22 19:49:00 +0600

    project done

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
 app.use(cors())
 app.use(express.json())
-   
+
 
 
 
@@ -32,24 +32,55 @@ async function run() {
 
         // database collection 
         const userCollection = client.db("Task_Manager").collection("users");
+        const taskCollection = client.db("Task_Manager").collection("tasks");
 
         // users api
-            app.post('/user', async (req, res) => {
-                const user = req.body;
-                const query = { email: user.email }
-                const existUser = await userCollection.findOne(query);
-                if (existUser) {
-                    return res.send({ message: 'User already exist in db' })
-                } else {
-                    const result = await userCollection.insertOne(user);
-                    res.send(result)
-                }
-            })
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existUser = await userCollection.findOne(query);
+            if (existUser) {
+                return res.send({ message: 'User already exist in db' })
+            } else {
+                const result = await userCollection.insertOne(user);
+                res.send(result)
+            }
+        })
 
 
+        // tasks apis 
+        app.post("/tasks", async (req, res) => {
+            const newData = req.body;
+            const result = await taskCollection.insertOne(newData)
+            res.send({ message: "success", data: result })
+        })
+
+        app.get("/task/:email", async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await taskCollection.find(filter).toArray();
+            res.send(result)
+        })
+
+        app.delete("/task/:id", async(req, res) => {
+            const id = req.params.id ;
+            const filter = {_id: new ObjectId(id)};
+            const result= await taskCollection.deleteOne(filter);
+            res.send(result)
+        })
 
 
-
+        app.patch('/task/category/:id', async(req, res) => {
+            const id = req.params.id;
+            const {droppableId} = req.body
+            const filter = {_id: new ObjectId(id)}
+            const updateDoc = {
+                $set: {
+                  category: droppableId
+                },
+              };
+            const result = await taskCollection.updateOne(filter, updateDoc) 
+        })
 
 
         // Send a ping to confirm a successful connection
